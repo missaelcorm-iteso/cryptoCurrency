5732a56fb93069ce14b87f768c38886b0bc02f1b
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2022-05-04 16:29:24 -0500

    Creating a try and a except

--- Modules/main.py
+++ Modules/main.py
@@ -9,21 +9,20 @@ def Main():
     # Loads the components
     appComponents()
     startHeaders()    
-#try:
-    # Defines the cryptos and get all data from them
-    wCryptos = cryptos()
+    try:
+        # Defines the cryptos and get all data from them
+        wCryptos = cryptos()
 
-    # Puts the data into a Data Frame
-    dFrame = dataFrame(wCryptos)
-    # Display the Data Frame
-    streamlitFrame(dFrame)
+        # Puts the data into a Data Frame
+        dFrame = dataFrame(wCryptos)
+        # Display the Data Frame
+        streamlitFrame(dFrame)
 
-    # Creates a CSV file
-    CSV = exportCSV(dFrame)
-    # Creates a button to download the CSV file
-    downloadCSV()
+        # Creates a CSV file
+        CSV = exportCSV(dFrame)
+        # Creates a button to download the CSV file
+        downloadCSV()
 
-    # Shows the select box and the expander
-    selectCrypto(dFrame)
-#except: st.text("Reload Page or delete cookies")
-
+        # Shows the select box and the expander
+        selectCrypto(dFrame)
+    except: st.text("Reload Page or delete cookies")
